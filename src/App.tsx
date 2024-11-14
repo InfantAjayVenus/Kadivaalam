cffcfb81713206b18df5e7ae2e9a5a1b45fc1968
Infant Ajay Venus <infantcsajay@outlook.com>
2024-11-14 20:29:19 +0530

    feat(App): Setup router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Planner } from './pages/Planner';
 export default function App() {
   return (
-    <main className="px-64">
+    <main className="px-96">
       <header className="border-b-4 border-primary">
         <h1
           className="text-3xl font-bold my-16"
@@ -8,6 +11,12 @@ export default function App() {
           role="title"
         >Kadivaalam</h1>
       </header>
+      <main role="main">
+        <Router>
+          <Route exact path="/" component={Home} />
+          <Route path="/session-planner" component={Planner} />
+        </Router>
+      </main>
     </main>
   );
 }
