fcfeab5e0341d3111ddaf367c2b908db2b0057e6
Infant Ajay Venus <infantcsajay@outlook.com>
2024-11-23 07:17:41 +0530

    feat(Planner): implement base planner page

---
+++ src/pages/Planner.test.tsx
@@ -0,0 +1,123 @@
+import "@testing-library/jest-dom";
+import { cleanup, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { afterEach } from "node:test";
+import { beforeEach, describe, expect, it } from "vitest";
+import { Planner } from "./Planner";
+
+describe("Planner", () => {
+    beforeEach(() => {
+        cleanup();
+        render(
+           <Planner /> 
+        );
+    })
+
+    it("should render", async () => {
+        const plannerRole = "planner";
+
+        await screen.findByRole(plannerRole);
+
+        expect(screen.getByRole(plannerRole)).toBeInTheDocument();
+    });
+
+    it("should render counter", async () => {
+        const counterRole = "task-counter";
+
+        await screen.findByRole(counterRole);
+
+        expect(screen.getByRole(counterRole)).toBeInTheDocument();
+        expect(screen.getByRole(counterRole)).not.toBeEmptyDOMElement();
+    });
+
+    it("should render help text when list is empty", async () => {
+        const counterRole = "task-counter";
+        const plannerRole = "task-advice-list";
+
+        await screen.findByRole(counterRole);
+
+        if (screen.getByRole(counterRole).textContent === "0") {
+            await screen.findByRole(plannerRole);
+            expect(screen.getByRole(plannerRole)).toBeInTheDocument();
+        }
+    });
+
+    it("should render button", async () => {
+        const buttonRole = "add-task-button";
+
+        await screen.findByRole(buttonRole);
+
+        expect(screen.getByRole(buttonRole)).toBeInTheDocument();
+    });
+
+    it("should render add  task form when button is clicked", async () => {
+        const addTaskFormRole = "add-task-form";
+        const buttonRole = "add-task-button";
+        userEvent.click(screen.getByRole(buttonRole));
+
+        await screen.findByRole(addTaskFormRole);
+
+        expect(screen.getByRole(addTaskFormRole)).toBeInTheDocument();
+    });
+
+    it("should disable add task button when add task form is open", async () => {
+        const addTaskFormRole = "add-task-form";
+        const buttonRole = "add-task-button";
+
+        await screen.findByRole(buttonRole);
+        userEvent.click(screen.getByRole(buttonRole));
+
+        await screen.findByRole(addTaskFormRole);
+
+        expect(screen.getByRole(buttonRole)).toBeDisabled();
+    });
+
+});
+
+describe("Add Task in Planner", () => {
+    const taskData = {
+        title: "Task Title Value",
+        description: "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    }
+
+    const helpTextRole = "task-advice-list";
+    const tasksListRole = "tasks-list";
+    const addTaskButtonRole = "add-task-button";
+    const taskCounterRole = "task-counter";
+    const taskTitleInputRole = "task-title-input";
+    const taskDescriptionInputRole = "task-description-input";
+    const submitTaskButtonRole = "submit-task-button";
+    // const abortAddTaskButtonRole = "abort-add-task-button";
+    beforeEach(async () => {
+        cleanup();
+        render(
+           <Planner /> 
+        )
+        await userEvent.click(screen.getByRole(addTaskButtonRole))
+        await userEvent.type(screen.getByRole(taskTitleInputRole), taskData.title);
+        await userEvent.type(screen.getByRole(taskDescriptionInputRole), taskData.description);
+        await userEvent.click(screen.getByRole(submitTaskButtonRole));
+
+    });
+
+    afterEach(() => {
+        cleanup();
+    });
+
+    it("should increment the counter when a task is submitted", async () => {
+
+        await screen.findByRole(taskCounterRole);
+        expect(screen.getByRole(taskCounterRole)).toHaveTextContent('1');
+    });
+
+    it("should not render the help text when a task is submitted", async () => {
+
+        expect(screen.queryByRole(helpTextRole)).not.toBeInTheDocument();
+    });
+
+    it("should render a task list when a task is submitted", async () => {
+        expect(screen.getByRole(tasksListRole)).toBeInTheDocument();
+    });
+
+})
+
---
+++ src/pages/Planner.tsx
@@ -0,0 +1,82 @@
+import { Play, Plus } from 'lucide-react';
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
+import { AddTask } from '../components/AddTask';
+import { TaskItem } from '../components/TaskItem';
+import { Task } from '../types/Task';
+
+interface PlannerProps {
+    taskId?: string;
+};
+
+export function Planner({ taskId = "" }: PlannerProps) {
+
+    const [isAddFormOpen, setIsAddFormOpen] = useState(false);
+    const [taskList, setTasksList] = useState<Task[]>([]);
+    
+    return (
+        <main role="planner" className='overflow-y-hidden'>
+            <p role="task-counter" className='text-2xl mt-2 mb-6'>{taskList.length}</p>
+            {(!isAddFormOpen && taskList.length === 0) &&
+                <ol
+                    role="task-advice-list"
+                    className='list-inside list-decimal text-center text-xl space-y-2 lg:text-2xl lg:space-y-8'
+                >
+                    <li>Create a task by adding a short title, a clear description and a time allocated for the task</li>
+                    <li>Be as specific as possible for each task</li>
+                    <li>If a task is too long, break it up into smaller tasks, typically not more than 40 minutes</li>
+                    <li>Take short breaks between tasks</li>
+                </ol>
+
+            }
+            {taskList.length > 0 &&
+                <ul role="tasks-list" className='max-h-96 mb-4'>
+                    {taskList.map(taskItem => (
+                        <TaskItem task={taskItem} key={taskItem.id} isActive={taskItem.id === taskId} />
+                    ))}
+                </ul>
+            }
+            {isAddFormOpen &&
+                <AddTask
+                    onAddTask={(title, description) => {
+                        const id = nanoid();
+                        const newTask: Task = {
+                            id,
+                            title,
+                            description,
+                            duration: {
+                                hours: 0,
+                                minutes: 0,
+                                seconds: 0
+                            }
+                        }
+
+                        setTasksList([...taskList, newTask]);
+
+                    }}
+                    onClose={() => setIsAddFormOpen(false)}
+                />
+            }
+            <div role='button-group' className='mt-16 flex flex-col space-y-6'>
+                <button
+                    role='add-task-button'
+                    onClick={() => setIsAddFormOpen(true)}
+                    disabled={isAddFormOpen}
+                    className='flex items-center justify-center space-x-2 mx-auto  border border-foreground rounded-2xl py-2 px-16 text-xl font-bold w-fit disabled:border-secondary disabled:text-secondary'
+                >
+                    Add Task <Plus size={32} />
+                </button>
+                {taskList.length > 0 &&
+                    <a href="/work-board" role="start-session-link">
+                        <button
+                            role='start-session-button'
+                            className='flex items-center justify-center space-x-2 mx-auto  border border-foreground rounded-2xl py-2 px-16 text-xl font-bold disabled:border-secondary disabled:text-secondary'
+                        >
+                            Start Session <Play size={32} />
+                        </button>
+                    </a>
+                }
+            </div>
+        </main>
+    );
+}
