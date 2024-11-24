147eb4ddbef4a37676c4d9b86fb2cbee1ca0bbc4
Infant Ajay Venus <infantcsajay@outlook.com>
2024-11-24 10:23:56 +0530

    enhance(taskStore): add updateTask operation to store

--- src/stores/taskStore.ts
+++ src/stores/taskStore.ts
@@ -8,12 +8,16 @@ interface TaskStoreState {
 interface TaskStoreAction {
     addTask: (task: Task) => void;
     removeTask: (id: string) => void;
+    updateTask: (task: Task) => void;
 }
 
 export const useTaskStore = create<TaskStoreState & TaskStoreAction>((set) => ({
     tasks: [] as Task[],
     addTask: (task: Task) => {
         set((state) => ({ tasks: [...state.tasks, task] }))
+    },
+    updateTask: (task: Task) => {
+        set((state) => ({ tasks: state.tasks.map((t) => t.id === task.id ? task : t) }))
     },
     removeTask: (id: string) => {
         set((state) => ({ tasks: state.tasks.filter((task) => task.id !== id) }))
