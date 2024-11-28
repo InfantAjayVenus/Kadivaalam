6ed6f2127c0806f6a1c7672f95a5e4aa7c19709c
Infant Ajay Venus <infantcsajay@outlook.com>
2024-11-28 15:29:35 +0530

    patch(TaskItem): update the template to display duration

--- src/components/TaskItem.test.tsx
+++ src/components/TaskItem.test.tsx
@@ -14,11 +14,11 @@ describe("TaskItem", () => {
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised",
         duration: {
             hours: 0,
-            minutes: 0,
-            seconds: 0,
+            minutes: 25,
         }
     }
     const taskTitleRole = 'task-item-title';
+    const taskDurationRole = 'task-duration';
 
     beforeEach(async () => {
         render(
@@ -32,5 +32,11 @@ describe("TaskItem", () => {
         await screen.findByRole(taskTitleRole);
 
         expect(screen.getByRole(taskTitleRole)).toHaveTextContent(taskData.title);
+    });
+
+    it("should render the duration of the task item", async () => {
+        await screen.findByRole(taskDurationRole);
+
+        expect(screen.getByRole(taskDurationRole)).toHaveTextContent("00h 25m");
     })
 })
--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -8,10 +8,14 @@ interface TaskItemProps {
 
 export function TaskItem({ task, isActive=false }: TaskItemProps) {
     return (
-        <li role="task-item" data-active={isActive} className="border border-primary data-[active=true]:bg-foreground data-[active=true]:text-background rounded-lg p-4 font-bold text-xl capitalize">
-            <Link to={`/session-planner/${task.id}`}>
+        <li 
+            role="task-item" 
+            data-active={isActive} 
+            className="flex justify-between gap-2 border border-primary data-[active=true]:bg-foreground data-[active=true]:text-background rounded-lg p-4 font-bold text-xl capitalize">
+            <Link to={`/session-planner/${task.id}`} className="flex-grow">
                 <h4 role="task-item-title">{task.title}</h4>
             </Link>
+            <p role="task-duration">{task.duration.hours.toString().padStart(2, '0')}h {task.duration.minutes.toString().padStart(2, '0')}m</p>
         </li>
     );
 
