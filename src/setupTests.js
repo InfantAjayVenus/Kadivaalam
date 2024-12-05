9cd4fc699915ed950c837cfa3ac1da92e6353ac0
Infant Ajay Venus <infantcsajay@outlook.com>
2024-12-05 06:44:25 +0530

    setup(setupTests): add resizeobserver mocking

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,11 @@
 import '@testing-library/jest-dom';
 import {render, screen} from "@testing-library/react";
-import {describe, it, beforeEach} from "vitest";
+import {describe, it, beforeEach, beforeAll} from "vitest";
+
+beforeAll(() => {
+    global.ResizeObserver = class ResizeObserver {
+        observe() {}
+        unobserve() {}
+        disconnect() {}
+    }
+});
